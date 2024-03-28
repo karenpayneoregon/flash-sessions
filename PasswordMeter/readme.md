616ebb4f15d35a0ed20ab0d9d0e5e2c5840cfc8d
Karen Payne <paynekaren@comcast.net>
2024-03-28 08:52:48 -0700

    new code

---
+++ PasswordMeter/readme.md
@@ -0,0 +1,9 @@
+﻿# About
+
+Original from https://dev.to/themodernweb/here-is-how-i-made-a-strong-password-checker-using-javascript-3m9o
+
+Ugly colors, Karen fixed that. Needs work with breakpoints for mobile.
+
+---
+
+https://codepen.io/coding_beast/pen/LYGrXde
---
+++ PasswordMeter/wwwroot/app.js
@@ -0,0 +1,39 @@
+// show password toggler
+
+let showPasswordBtn = document.querySelector(".show-password");
+let passwordInp = document.querySelector(".password-input");
+let passwordChecklist = document.querySelectorAll(".list-item");
+
+showPasswordBtn.addEventListener("click", () => {
+    // toggle icon
+
+    showPasswordBtn.classList.toggle("fa-eye");
+    showPasswordBtn.classList.toggle("fa-eye-slash");
+
+    passwordInp.type = passwordInp.type === "password" ? "text" : "password";
+});
+
+// string password validation
+
+let validationRegex = [
+    { regex: /.{24,}/ }, // min 8 letters,
+    { regex: /[0-9]/ }, // numbers from 0 to 9
+    { regex: /[a-z]/ }, // letters from a - z (lowercase)
+    { regex: /[A-Z]/ }, // letters from A-Z (uppercase),
+    { regex: /[^A-Za-z0-9]/ } // special characters
+];
+
+passwordInp.addEventListener("keyup", () => {
+
+    validationRegex.forEach((item, index) => {
+
+        const isValid = item.regex.test(passwordInp.value);
+
+        if (isValid) {
+            passwordChecklist[index].classList.add("checked");
+        } else {
+            passwordChecklist[index].classList.remove("checked");
+        }
+    });
+
+});
