5317ac06cba23b9870f0dff4620ab9ec6b8f6ede
Karen Payne <paynekaren@comcast.net>
2024-04-10 07:56:30 -0700

    :curly_loop: misc

---
+++ BasicsFrontendApp/Classes/OtherStuff/Other.cs
@@ -0,0 +1,43 @@
+﻿using BasicsFrontendApp.Models;
+using Newtonsoft.Json;
+using System.Xml;
+
+namespace BasicsFrontendApp.Classes.OtherStuff;
+
+/// <summary>
+/// Not part of the learning path, Karen tinkering
+/// </summary>
+internal class Other
+{
+    /// <summary>
+    /// Given a list of <see cref="Taxpayer"/> creates an XML file
+    /// </summary>
+    /// <param name="taxpayersList">List with at least one record</param>
+    private static void ConvertDataToXml(List<Taxpayer> taxpayersList)
+    {
+        string json = JsonConvert.SerializeObject(
+            taxpayersList, Newtonsoft.Json.Formatting.Indented, new TaxpayerJsonConverter(typeof(Taxpayer)));
+        ConvertJsonToXml(json);
+        SpectreConsoleHelpers.ShowTaxpayers(taxpayersList);
+    }
+    public static void ConvertJsonToXml(string text)
+    {
+
+        string json = $$"""
+                            {
+                                '?xml': {
+                                '@version': '1.0',
+                                '@standalone': 'no'
+                            },
+                            'Taxpayers':
+                            {
+                                'Taxpayer':
+                                 {{text}}
+                              }
+                            }
+                        """;
+
+        XmlDocument doc = JsonConvert.DeserializeXmlNode(json)!;
+        doc.Save("People.xml");
+    }
+}
--- BasicsFrontendApp/Program.cs
+++ BasicsFrontendApp/Program.cs
@@ -12,33 +12,5 @@ internal partial class Program
     static void Main(string[] args)
     {
         List<Taxpayer> taxpayersList= BogusOperations.TaxpayerList();
-
-        string json = JsonConvert.SerializeObject(
-            taxpayersList, Newtonsoft.Json.Formatting.Indented, new TaxpayerJsonConverter(typeof(Taxpayer)));
-        ConvertJsonToXml(json);
-        SpectreConsoleHelpers.ShowTaxpayers(taxpayersList);
-        //Console.ReadLine();
-    }
-
-
-    public static void ConvertJsonToXml(string text)
-    {
-
-        string json = $$"""
-                            {
-                                '?xml': {
-                                '@version': '1.0',
-                                '@standalone': 'no'
-                            },
-                            'Taxpayers':
-                            {
-                                'Taxpayer':
-                                 {{text}}
-                              }
-                            }
-                        """;
-
-        XmlDocument doc = JsonConvert.DeserializeXmlNode(json)!;
-        doc.Save("People.xml");
     }
 }
